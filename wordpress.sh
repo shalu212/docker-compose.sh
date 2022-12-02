23a92a8c60c91278d8211509e4b7fe95af55124b
shalu212 <111748599+shalu212@users.noreply.github.com>
2022-12-02 16:32:46 +0530

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
-sudo yum -y install git
-git clone https://github.com/jayeshrocks34/wordpress.git
-sudo yum -y install docker
+sudo yum install git -y
+sudo amazon-linux-extras install docker -y
+sudo service docker start
 sudo usermod -a -G docker ec2-user
-sudo systemctl start docker
-sudo systemctl enable docker
-sudo curl -L https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+sudo chkconfig docker on
+sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
-cd wordpress1
-sudo docker-compose up -d
+docker-compose version
+sudo git clone https://github.com/amar-m-cloud/docker-compose.git
+sudo chmod 666 /var/run/docker.sock
+cd docker-compose
+docker-compose up -d
