453408a028ad6e034d855b27d89f98872efca721
Shalini <111748599+shalu212@users.noreply.github.com>
2023-02-21 15:47:41 +0530

    Update wordpresss.sh

--- wordpresss.sh
+++ wordpresss.sh
@@ -6,8 +6,9 @@ sudo usermod -a -G docker ec2-user
 sudo chkconfig docker on
 sudo curl -L https://github.com/docker/compose/releases/latest/download/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
-docker-compose version
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
 sudo git clone https://github.com/shalu212/wordpress-script.git
 sudo chmod 666 /var/run/docker.sock
 cd wordpress-script
-
+sudo docker-compose up -d
